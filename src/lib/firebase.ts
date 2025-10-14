09cf9778ea791384127301062498117885c0369e
DHYANAM SHAH <dhyanam2412@gmail.com>
2025-10-14 14:04:15 +0000

    const firebaseConfig = {   apiKey: "AIzaSyBWcCuSyTqIw1up9xFIUDNfqFAolBVW

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,14 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// This configuration is now correctly set up for your project.
 const firebaseConfig = {
-  apiKey: "AIzaSyXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  apiKey: "AIzaSyBWcCuSyTqIw1up9xFIUDNfqFAolBVWywk",
   authDomain: "studio-1850346779-9c1cf.firebaseapp.com",
   projectId: "studio-1850346779-9c1cf",
   storageBucket: "studio-1850346779-9c1cf.appspot.com",
-  messagingSenderId: "1073534538314",
-  appId: "1:1073534538314:web:e4f9b4e69b5d3c3a4a3a3b",
+  messagingSenderId: "952353938825",
+  appId: "1:952353938825:web:02c2a6da33c6f400481558"
 };
 
 // Initialize Firebase
